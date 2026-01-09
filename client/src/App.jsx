f0e8c2d64d8268f7337af1e18325d2e04b35eb17
kminz-saloni <karanminj9717@gmail.com>
2026-01-10 01:05:09 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -23,6 +24,7 @@ function App() {
         </Routes>
       </BrowserRouter>
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }
